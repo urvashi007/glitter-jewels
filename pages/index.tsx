ad97230a6be63d0c143ace0f6b73b241ec70cd78
mac <mac@macs-MacBook-Pro.local>
2025-06-26 01:13:10 +0530

    changes of ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -109,7 +109,7 @@ export default function HomePage() {
 
       <FadeInOnScroll>
  
-      <Container maxWidth="lg" sx={{ p: 0 }}>
+      <Container maxWidth="lg">
         <Box maxWidth="lg" sx={{ mx: "auto", py: 10, background:customVars.background.whitebg }}>
           <CardCollection
           
